e6f3992adf285aeda5656a286aca007d97120e14
wenmu01 <34831393+wenmu01@users.noreply.github.com>
2018-01-12 14:28:56 +0800

    add all and commit all

---
+++ Test/src/com/alibaba/dao/DataCreat.java
@@ -0,0 +1,16 @@
+package com.alibaba.dao;
+
+import org.junit.Test;
+
+public class DataCreat {
+	
+	@Test
+	public void test() throws InterruptedException{
+		
+		new Thread();
+		Thread.sleep(1000);
+
+		
+	}
+	
+}
